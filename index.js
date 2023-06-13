9d8d43c607f606cb1f1d405056c4de67d06f3a5d
Joseph <jcolitto@mac.com>
2023-06-13 14:41:52 -0500

    Need to revisit this onegit add .

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+  if (pickupLocation < 42) {
+    return 42 - pickupLocation;
+  } else {
+    return pickupLocation - 42;
+  }
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+  return distanceFromHqInBlocks(pickupLocation) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  if (start < destination) {
+    return (destination - start) * 264;
+  } else {
+    return (start - destination) * 264;
+  }
+}
+
+function calculatesFarePrice(start, destination) {
+  let distance = distanceTravelledInFeet(start, destination);
+  if (distance <= 400) {
+    return 0;
+  }
+  if (distance >= 400 && distance <= 2000) {
+    return (distance - 400) * 0.02;
+  } else if (distance >= 2000 && distance <= 2500) {
+    return 25;
+  } else if (distance > 2500) {
+    return "cannot travel that far";
+  }
+}
